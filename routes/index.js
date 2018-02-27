ca10b38f8473da0be70367988908419921ac53b2
Paul Ballas <paul.ballas@gmail.com>
2018-02-27 15:06:34 -0700

    push hellowalelt

--- routes/index.js
+++ routes/index.js
@@ -1,51 +1,55 @@
-var express = require('express');
-var nodemailer = require('nodemailer');
-var projects = require('../data/data.json')
-var comics = require('../data/comics.json')
-var router = express.Router();
+var express = require('express'),
+    nodemailer = require('nodemailer'),
+    projects = require('../data/data.json'),
+    comics = require('../data/comics.json'),
+    router = express.Router();
 
 router.get('/', function(req, res, next) {
-  res.render('home', { title: 'Paul Ballas' });
+  res.render('home',{ title: 'Paul Ballas' });
 });
 
 router.get('/portfolio', function(req, res, next) {
-  res.render('portfolio', { title: 'Paul Ballas - Portfolio', projects });
+  res.render('portfolio',{ title: 'Paul Ballas - Portfolio', projects });
 });
 
 router.get('/portfolio/process', function(req, res, next) {
-  res.render('process', { title: 'Paul Ballas - Process' });
+  res.render('process',{ title: 'Paul Ballas - Process' });
 });
 
 router.get('/portfolio/abe', function(req, res, next) {
-  res.render('abe', { title: 'Paul Ballas' });
+  res.render('abe',{ title: 'Paul Ballas - ABE' });
 });
 
 router.get('/portfolio/class', function(req, res, next) {
-  res.render('class', { title: 'Paul Ballas' });
+  res.render('class',{ title: 'Paul Ballas - ClassScout' });
 });
 
 router.get('/portfolio/illustrations', function(req, res, next) {
-  res.render('illustrations', { title: 'Paul Ballas', comics });
+  res.render('illustrations',{ title: 'Paul Ballas - Illustrations', comics });
 });
 
 router.get('/portfolio/marigold', function(req, res, next) {
-  res.render('marigold', { title: 'Paul Ballas', comics });
+  res.render('marigold',{ title: 'Paul Ballas - Marigold' });
 });
 
 router.get('/portfolio/pana', function(req, res, next) {
-  res.render('pana', { title: 'Paul Ballas', comics });
+  res.render('pana',{ title: 'Paul Ballas - Pana' });
 });
 
 router.get('/portfolio/personas', function(req, res, next) {
-  res.render('personas', { title: 'Paul Ballas', comics });
+  res.render('personas',{ title: 'Paul Ballas - Personas' });
 });
 
 router.get('/portfolio/ytb', function(req, res, next) {
-  res.render('ytb', { title: 'Paul Ballas', comics });
+  res.render('ytb',{ title: 'Paul Ballas - YTB' });
+});
+
+router.get('/portfolio/hw', function(req, res, next) {
+  res.render('hw',{ title: 'Paul Ballas - HelloWallet' });
 });
 
 router.get('*', function(req, res, next) {
-  res.render('err', { title: 'Whoops!' });
+  res.render('err',{ title: 'Whoops!' });
 });
 
 module.exports = router;
